7b7f9c2fd6ed492f7b1c93eef041ed5e3ee2a677
Nao <naounderscore@teslaitstudios.com>
2021-12-04 22:37:53 +0100

    Added AssetBundle, Authentication and QueryProcessor patches

---
+++ Scope.Client/Events/Patches/Authentication/SignActionPatch.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SignActionPatch.cs" company="Scope SL">
+// Copyright (c) Scope SL. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Scope.Client.Events.Patches.Authentication
+{
+#pragma warning disable SA1600 // Elements should be documented
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+
+    using HarmonyLib;
+    using Scope.Client.API.Features;
+
+    /// <summary>
+    /// Patches <see cref="CentralAuthManager.Sign"/>.
+    /// </summary>
+    [HarmonyPatch(typeof(CentralAuthManager), nameof(CentralAuthManager.Sign))]
+    internal class SignActionPatch
+    {
+        public static bool Prefix(ref string __result, string ticket)
+        {
+            Log.Info($"CentralAuthManager::Sign action: {ticket}");
+            __result = string.Empty;
+            return false;
+        }
+    }
+}
--- Scope.Client/Events/Patches/TransmissionNetwork/TargetPrintOnConsolePatch.cs
+++ Scope.Client/Events/Patches/TransmissionNetwork/TargetPrintOnConsolePatch.cs
@@ -1,15 +1,13 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="TransmissionNetwork.cs" company="Scope SL">
+// <copyright file="TargetPrintOnConsolePatch.cs" company="Scope SL">
 // Copyright (c) Scope SL. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
 // -----------------------------------------------------------------------
 
-namespace Scope.Client.Events.Patches
+namespace Scope.Client.Events.Patches.TransmissionNetwork
 {
-#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
 #pragma warning disable SA1600 // Elements should be documented
-#pragma warning disable SA1401 // Fields should be private
 
     using HarmonyLib;
     using Scope.Client.API.Extensions;
@@ -18,20 +16,12 @@ namespace Scope.Client.Events.Patches
     using UnhollowerBaseLib;
 
     /// <summary>
-    /// Patches <see cref="GameConsoleTransmission.Start"/> and
-    /// <see cref="GameConsoleTransmission.UserCode_TargetPrintOnConsole(Mirror.NetworkConnection, Il2CppStructArray{byte}, bool)"/>.
+    /// Patches <see cref="GameConsoleTransmission.UserCode_TargetPrintOnConsole(Mirror.NetworkConnection, Il2CppStructArray{byte}, bool)"/>.
     /// </summary>
-    internal class TransmissionNetwork
+    [HarmonyPatch(typeof(GameConsoleTransmission), nameof(GameConsoleTransmission.UserCode_TargetPrintOnConsole))]
+    internal class TargetPrintOnConsolePatch
     {
-        internal static GameConsoleTransmission Transmission = null;
-
-        [HarmonyPatch(typeof(GameConsoleTransmission), nameof(GameConsoleTransmission.Start))]
-        [HarmonyPrefix]
-        internal static void TransmissionStart(GameConsoleTransmission __instance) => Transmission = __instance;
-
-        [HarmonyPatch(typeof(GameConsoleTransmission), nameof(GameConsoleTransmission.UserCode_TargetPrintOnConsole))]
-        [HarmonyPrefix]
-        internal static bool TargetPrintOnConsole(Il2CppStructArray<byte> data, bool encrypted)
+        internal static bool Prefix(Il2CppStructArray<byte> data, bool encrypted)
         {
             if (data == null)
                 return false;

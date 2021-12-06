f629e6acd8c25f8ecddbcdaf39df5bd2e1f4175e
xRoier <rogerazomar@gmail.com>
2021-12-07 00:41:32 +0100

    Oops

--- Scope.Client/Loader/Loader.cs
+++ Scope.Client/Loader/Loader.cs
@@ -66,7 +66,7 @@ namespace Scope.Client.Loader
         {
             try
             {
-                return Assembly.UnsafeLoadFrom(path); // Assembly.Load(File.ReadAllBytes(path));
+                return Assembly.UnsafeLoadFrom(path);
             }
             catch (Exception exception)
             {

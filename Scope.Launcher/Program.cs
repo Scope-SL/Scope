cde9922e256f65586b3d7c8195da4a1996c0250e
moddedmcplayer <moddedmcplayer@gmail.com>
2022-05-29 08:25:16 +0200

    funni launcher

---
+++ Scope.Launcher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Scope.Launcher
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            [DllImport("UnityPlayer.dll", CharSet = CharSet.Unicode)]
+            static extern IntPtr UnityMain(IntPtr instance, IntPtr previous_instance, string[] command_line, int show_command_line);
+
+            UnityMain(Marshal.GetHINSTANCE(typeof(Program).Module), IntPtr.Zero, args, 1);
+        }
+    }
+}

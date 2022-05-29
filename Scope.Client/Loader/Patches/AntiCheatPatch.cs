ef51d5e767c688dda90ba7ce4adb674ce80231ef
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2022-05-29 17:03:51 +0200

    Dev (#18)
    * funni launcher
    
    * fix
    
    * funny
    
    Co-authored-by: moddedmcplayer <moddedmcplayer@gmail.com>

---
+++ Scope.Client/Loader/Patches/AntiCheatPatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace Scope.Client.Loader.Patches
+{
+    /// <summary>
+    /// Patches the game to prevent it from loading the AntiCheat.
+    /// </summary>
+    // [HarmonyPatch(typeof())]
+    // public class AntiCheatPatch
+    // {
+    //     
+    // }
+}

d6abc7c69a786aa9d29b2af6ed332c351c3b690d
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2022-06-06 21:52:35 +0200

    Revert "Scope.Launcher moved to .NET 6, C# 10." (#19)
    This reverts commit 4c2c5c3035c603b51f56b53852e704fe911e59c3.
    
    Co-authored-by: moddedmcplayer <moddedmcplayer@gmail.com>

--- Scope.Launcher/Program.cs
+++ Scope.Launcher/Program.cs
@@ -1,6 +1,16 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
-[DllImport("UnityPlayer.dll", CharSet = CharSet.Unicode)]
-static extern IntPtr UnityMain(IntPtr instance, IntPtr previous_instance, string[] command_line, int show_command_line);
+namespace Scope.Launcher
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            [DllImport("UnityPlayer.dll", CharSet = CharSet.Unicode)]
+            static extern IntPtr UnityMain(IntPtr instance, IntPtr previous_instance, string[] command_line, int show_command_line);
 
-UnityMain(Marshal.GetHINSTANCE(typeof(Program).Module), IntPtr.Zero, args, 1);
+            UnityMain(Marshal.GetHINSTANCE(typeof(Program).Module), IntPtr.Zero, args, 1);
+        }
+    }
+}
